cb826b57b9e0c4a62e858a059da733f1773b052a
Michael <michael.arn@live.fr>
2021-03-21 22:56:58 +0100

    correct few things

--- GameJamProject/Assets/Scripts/Managers/GameManager.cs
+++ GameJamProject/Assets/Scripts/Managers/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : Singleton<GameManager>
     private GameObject fxSlowmo = null;
     private float customDT = 1.0f;
     private float timerSlowMo = 5.0f;
-    private bool startSlowMo = true;
+    private bool startSlowMo = false;
     private int nbKillRequired = 20;
     private static int score = 0;
     private static string[] highScoresName = new string[5];
